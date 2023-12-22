98973a3c6e2972e6ac06e3bc2df88fa9d3fed089
amirani <amirani>
2023-12-22 20:23:44 +0400

    add Player class

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,6 +1,10 @@
 import Phaser from "phaser";
+import { Player } from "../entities/Player";
+import { SpriteWithDynamicBody } from "../types";
 
 class PlayScene extends Phaser.Scene {
+  player: Player;
+  startTrigger: SpriteWithDynamicBody;
   constructor() {
     super("PlayScene");
   }
@@ -12,10 +16,24 @@ class PlayScene extends Phaser.Scene {
   create() {
     this.createEnvironment();
     this.createPlayer();
+
+    this.startTrigger = this.physics.add.sprite(0, 10, null).setOrigin(0,1).setAlpha(0)
+    this.registerPlayerControl()
+
+    this.physics.add.overlap(this.startTrigger, this.player, () => {
+      console.log('overlap')
+    })
   }
 
   createPlayer() {
-    this.add.sprite(0, this.gameHeight, "dino-idle").setOrigin(0, 1);
+    this.player = new Player(this, 0, this.gameHeight);  
+  }
+
+  registerPlayerControl() {
+     const spaceBar =  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+     spaceBar.on("down", () => {
+      this.player.setVelocityY(-1600);
+    });
   }
 
   createEnvironment() {
